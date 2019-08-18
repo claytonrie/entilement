e2c50c96378b3624320f7e771e85a3b7fd22e1ee
Claire Riegger <jonfieldthe2nd@gmail.com>
2019-08-18 12:28:49 -0400

    Revise text.js
    Refactor textDisplay to texts

--- src/text.js
+++ src/text.js
@@ -1,4 +1,4 @@
-var textDisplay = new (class {
+var texts = new (class {
     constructor() {
         this.char = [];
         this.x = [];
@@ -23,15 +23,15 @@ class TextAnimation {
     constructor (txt, x, y, color, size) {
         let i = txt.length - 1;
         for (; i >= 0; i -= 1) {
-            textDisplay.char.push(txt[i]);
-            textDisplay.x.push(x + 0.5 * size * i);
-            textDisplay.y.push(y);
-            textDisplay.pos.push(i);
-            textDisplay.size.push(size);
-            textDisplay.color.push(color);
-            textDisplay.time.push(0);
+            texts.char.push(txt[i]);
+            texts.x.push(x + 0.5 * size * i);
+            texts.y.push(y);
+            texts.pos.push(i);
+            texts.size.push(size);
+            texts.color.push(color);
+            texts.time.push(0);
         }
-        textDisplay.length += txt.length;
+        texts.length += txt.length;
     }
 
     static advanceDraw(dt, ind) {
@@ -41,13 +41,13 @@ class TextAnimation {
             phase3 = 64 + 1536,
             phase4 = 4096 - 512,
             phase5 = 4096;
-        Drawer.size = textDisplay.size[ind];
-        textDisplay.time[ind] += dt;
+        Drawer.size = texts.size[ind];
+        texts.time[ind] += dt;
         let op, inColor, outColor;
-        let eTime = textDisplay.time[ind] - 64 * textDisplay.pos[ind],
+        let eTime = texts.time[ind] - 64 * texts.pos[ind],
             phTime;
-        let txt = textDisplay.char[ind], x = textDisplay.x[ind],
-            y = textDisplay.y[ind], sz = textDisplay.size[ind];
+        let txt = texts.char[ind], x = texts.x[ind],
+            y = texts.y[ind], sz = texts.size[ind];
         Drawer.ctx.font = `${sz * Drawer.scale}px monospace`;
         if (eTime < phase5) {
             Drawer.lineWidth = sz / 40;
@@ -56,35 +56,35 @@ class TextAnimation {
                 Drawer.color = "#DDD";
                 Drawer.drawText(true, txt, x, y - sz * phTime / 512,
                     1 - phTime / 512);
-                Drawer.color = textDisplay.color[ind];
+                Drawer.color = texts.color[ind];
                 Drawer.drawText(false, txt, x, y - sz * phTime / 512,
                     1 - phTime / 512);
-            } else if (eTime > phase3 || textDisplay.time[ind] < 0) {
+            } else if (eTime > phase3 || texts.time[ind] < 0) {
                 Drawer.color = "#DDD";
                 Drawer.drawText(true, txt, x, y);
-                Drawer.color = textDisplay.color[ind];
+                Drawer.color = texts.color[ind];
                 Drawer.drawText(false, txt, x, y);
-                if (textDisplay.time[ind] < 0 && !textDisplay.stopTime) {
-                    textDisplay.time[ind] = phase4;
-                } else if (textDisplay.time[ind] > 0 && textDisplay.stopTime) {
-                    textDisplay.time[ind] = -Infinity;
+                if (texts.time[ind] < 0 && !texts.stopTime) {
+                    texts.time[ind] = phase4;
+                } else if (texts.time[ind] > 0 && texts.stopTime) {
+                    texts.time[ind] = -Infinity;
                 }
             } else if (eTime > phase2) {
                 phTime = eTime - phase2;
                 // Real text
                 Drawer.color = "#DDD";
                 Drawer.drawText(true, txt, x, y, phTime / 512);
-                Drawer.color = textDisplay.color[ind];
+                Drawer.color = texts.color[ind];
                 Drawer.drawText(false, txt, x, y, phTime / 512);
                 // Miscolored text
-                Drawer.color = textDisplay.color[ind];
+                Drawer.color = texts.color[ind];
                 Drawer.drawText(true, txt, x, y, 1 - phTime / 512);
                 Drawer.color = "#888";
                 Drawer.drawText(false, txt, x, y, 1 - phTime / 512);
             } else if (eTime > phase1) {
                 phTime = eTime - phase1;
                 // Miscolored text
-                Drawer.color = textDisplay.color[ind];
+                Drawer.color = texts.color[ind];
                 Drawer.drawText(true, txt, x, y, phTime / 512);
                 Drawer.color = "#888";
                 Drawer.drawText(false, txt, x, y, phTime / 512);
@@ -98,14 +98,14 @@ class TextAnimation {
                     phTime / 512);
             }
         } else {
-            textDisplay.char.splice(ind, 1);
-            textDisplay.x.splice(ind, 1);
-            textDisplay.y.splice(ind, 1);
-            textDisplay.pos.splice(ind, 1);
-            textDisplay.color.splice(ind, 1);
-            textDisplay.size.splice(ind, 1);
-            textDisplay.time.splice(ind, 1);
-            textDisplay.length -= 1;
+            texts.char.splice(ind, 1);
+            texts.x.splice(ind, 1);
+            texts.y.splice(ind, 1);
+            texts.pos.splice(ind, 1);
+            texts.color.splice(ind, 1);
+            texts.size.splice(ind, 1);
+            texts.time.splice(ind, 1);
+            texts.length -= 1;
         }
     }
 }
