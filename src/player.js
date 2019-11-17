e09fc6877126f24ad7a8697236152ea44d62276a
Claire Riegger <jonfieldthe2nd@gmail.com>
2019-11-16 22:06:58 -0500

    Update player.js
    Recode the "smooth transit" class to follow a path dictated strictly by time instead of a differential equation.

--- src/player.js
+++ src/player.js
@@ -3,10 +3,9 @@ class TransitObj {
     constructor (x, y, tx = x, ty = y, vx = 0, vy = 0) {
         this.pos = new Vec2(x, y);
         this.tar = new Vec2(tx, ty);
-        this.vel = new Vec2(vx, vy);
-        this.acc = new Vec2();
+        this.time = 0;
         this.slopeVar = new Vec2(null, null);
-        this.slopeCounter = new Vec2(null, null);
+        this.distVar = new Vec2(null, null);
         this.atTarget = false;
     }
     
@@ -17,51 +16,40 @@ class TransitObj {
     }
 
     resetTransition (atTarget = false) {
-        this.slopeVar.x = null; this.slopeVar.y = null;
-        this.slopeCounter.x = null;
-        this.slopeCounter.y = null;
+        const TIME_DUR = 256;
+        if (!atTarget) {
+            let velx, vely;
+            if (this.time < TIME_DUR) { 
+                let EXP = Math.exp(1 + TIME_DUR / (this.time - TIME_DUR));
+                velx = this.distVar.x * EXP * (this.slopeVar.x - 
+                    TIME_DUR * (this.slopeVar.x * this.pos.x + 1) / (this.pos.x - TIME_DUR) / (this.pos.x - TIME_DUR));
+                vely = this.distVar.y * EXP * (this.slopeVar.y - 
+                    TIME_DUR * (this.slopeVar.y * this.pos.y + 1) / (this.pos.y - TIME_DUR) / (this.pos.y - TIME_DUR));
+            } else {
+                velx = vely = 0;
+            }
+            this.distVar = this.pos.to(this.tar);
+            this.time = 0;
+            this.slopeVar.x = velx / this.distVar.x + 1 / TIME_DUR;
+            this.slopeVar.y = vely / this.distVar.y + 1 / TIME_DUR;
+        } else {
+            this.pos.x = this.tar.x;
+            this.pos.y = this.tar.y;
+            this.time = TIME_DUR;
+        }
         this.atTarget = atTarget;
     }
 
     smoothTransition (dt) {
-        if (Vec2.sqDist(this.pos, this.tar) < 1 / Drawer.scale) {
-            this.vel.x = this.vel.y = 0;
-            this.acc.x = this.acc.y = 0;
-            this.pos.x = this.tar.x;
-            this.pos.y = this.tar.y;
+        const TIME_DUR = 256;
+        if (this.time >= TIME_DUR) {
             this.resetTransition(true);
         } else {
-            const time = 500 / TMULT / 8;
-            let dist = this.pos.to(this.tar);
-            let vx = this.vel.x, vy = this.vel.y;
-            if (typeof this.slopeVar.x !== "number") {
-                let slope = Math.abs(vx / dist.x) + (1 / time);
-                this.slopeVar.x = isNaN(slope) ? 0 : slope;
-                slope = Math.abs(vy / dist.y) + (1 / time);
-                this.slopeVar.y = isNaN(slope) ? 0 : slope;
-                this.slopeCounter.x = 1; this.slopeCounter.y = 1;
-            }
-            if (Math.abs(dist.x) < 0.5 / Drawer.scale) {
-                this.acc.x = 0;
-                this.vel.x = 0;
-            } else {
-                let basex = vx - this.slopeVar.x * dist.x / this.slopeCounter.x;
-                let divx = Math.abs(basex / dist.x);
-                let accx = (2 * Math.sqrt(divx / time) - divx +
-                    (2 * this.slopeVar.x / this.slopeCounter.x)) * basex;
-                this.acc.x = isNaN(accx) ? 0 : -accx;
-            }
-            if (Math.abs(dist.y) < 0.5 / Drawer.scale) {
-                this.acc.y = 0;
-                this.vel.y = 0;
-            } else {
-                let basey = vy - this.slopeVar.y * dist.y / this.slopeCounter.y;
-                let divy = Math.abs(basey / dist.y);
-                let accy = (2 * Math.sqrt(divy / time) - divy +
-                    (2 * this.slopeVar.y / this.slopeCounter.y)) * basey;
-                this.acc.y = isNaN(accy) ? 0 : -accy;
-            }
-            this.slopeCounter.addEq(this.slopeVar.scale(dt / TMULT));
+            let disp = this.pos.to(this.tar), EXP = Math.exp(1 + TIME_DUR / (this.time - TIME_DUR));
+            let newX = this.distVar.x * (this.slopeVar.x * disp.x + 1) * EXP,
+                newY = this.distVar.y * (this.slopeVar.y * disp.y + 1) * EXP;
+            this.pos.x = newX; this.pos.y = newY;
+            this.time += dt;
         }
     }
 }
