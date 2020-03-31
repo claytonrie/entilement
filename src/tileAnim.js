391e3fa6a2eb3a385c3433bbf5062e1aa5d6a279
Claire Riegger <jonfieldthe2nd@gmail.com>
2020-03-31 15:47:25 -0500

    Update tileAnim.js
    Change tile animation types from a magic number to an enum
    Change animation functions from enumerated named functions to an array of functions

--- src/tileAnim.js
+++ src/tileAnim.js
@@ -1,10 +1,9 @@
-// Animation types:
-//     0, 1, 2, 4, 6, 7, 8, 9, 10, 11
 class TileAnimation {
     constructor (type, x, y) {
-        tiles.atype.push(type);
-        tiles.ax.push(x); tiles.ay.push(y);
-        tiles.atime.push(0);
+        tiles.atype[tiles.alength] = type;
+        tiles.ax[tiles.alength] = x;
+        tiles.ay[tiles.alength] = y;
+        tiles.atime[tiles.alength] = 0;
         tiles.alength += 1;
         return true;
     }
@@ -41,71 +40,86 @@ class TileAnimation {
             16 - 2 * shrink, 16 - 2 * shrink);
         return false;
     }
-
-    static animate0 (dt, ind) { return TileAnimation.shrink(dt, ind, "blue"); }
-    static animate1 (dt, ind) { return TileAnimation.shrink(dt, ind, "red"); }
-    static animate2 (dt, ind) { return TileAnimation.shrink(dt, ind, "green"); }
-    static animate4 (dt, ind) {
-        let op = 1 - (tiles.atime[ind] / 512);
-        Drawer.lineWidth = 3;
-        if (op <= 0) { return true; }
-        Drawer.setLineWidth(3).setColor(Tile.outerColorTable[4]);
-        Drawer.drawRect(true, tiles.ax[ind], tiles.ay[ind], 16, 16, op);
-        return false;
-    }
-    static animate6 (dt, ind) { return TileAnimation.shrinkFill(dt, ind, "blue"); }
-    static animate7 (dt, ind) { return TileAnimation.shrinkFill(dt, ind, "red"); }
-    static animate8 (dt, ind) {
-        let op = 1 - (tiles.atime[ind] / 512);
-        if (op <= 0) { return true; }
-        Drawer.color = Tile.innerColorTable[8];
-        Drawer.drawRect(false, tiles.ax[ind], tiles.ay[ind], 16, 16, 0.75 * op);
-        return false;
-    }
-    static animate9 (dt, ind) {
-        let op = 1 - (tiles.atime[ind] / 512);
-        if (op <= 0) { return true; }
-        Drawer.setLineWidth(5 * op + 3).setColor(Tile.innerColorTable[9]);
-        Drawer.drawRect(true, tiles.ax[ind], tiles.ay[ind], 16, 16, 0.75);
-        Drawer.setLineWidth(3).setColor(Tile.outerColorTable[9]);
-        Drawer.drawRect(true, tiles.ax[ind], tiles.ay[ind], 16, 16, op);
-        return false;
-    }
-    static animate10 (dt, ind) {
-        let op = 1 - (tiles.atime[ind] / 512);
-        if (op <= 0) { return true; }
-        Drawer.setLineWidth(5 * op + 3).setColor(Tile.innerColorTable[10]);
-        Drawer.drawRect(true, tiles.ax[ind], tiles.ay[ind], 16, 16, 0.75);
-        Drawer.setLineWidth(3).setColor(Tile.outerColorTable[10]);
-        Drawer.drawRect(true, tiles.ax[ind], tiles.ay[ind], 16, 16, op);
-        return false;
-    }
-    static animate11 (dt, ind) { return TileAnimation.shrink(dt, ind, "#FF0"); }
-    static animate13 (dt, ind) {
-        let op = 1 - (tiles.atime[ind] / 512) / 2,
-            pos = 8 * tiles.atime[ind] / 512 / 2;
-        if (op <= 0) { return true; }
-        Drawer.setColor(Tile.outerColorTable[13]);
-        let rand1 = 2 * TileAnimation.RNG(tiles.ax[ind], tiles.ay[ind], 0, 2) - 1,
-        	rand2 = 2 * TileAnimation.RNG(tiles.ax[ind], tiles.ay[ind], 1, 2) - 1,
-        	rand3 = 2 * TileAnimation.RNG(tiles.ax[ind], tiles.ay[ind], 2, 2) - 1,
-        	rand4 = 2 * TileAnimation.RNG(tiles.ax[ind], tiles.ay[ind], 3, 2) - 1,
-            rand5 = 2 * TileAnimation.RNG(tiles.ax[ind], tiles.ay[ind], 4, 2) - 1,
-        	rand6 = 2 * TileAnimation.RNG(tiles.ax[ind], tiles.ay[ind], 5, 2) - 1,
-        	rand7 = 2 * TileAnimation.RNG(tiles.ax[ind], tiles.ay[ind], 6, 2) - 1,
-        	rand8 = 2 * TileAnimation.RNG(tiles.ax[ind], tiles.ay[ind], 7, 2) - 1;
-        Drawer.drawRect(false, tiles.ax[ind] + rand1 * pos, tiles.ay[ind] + rand5 * pos, 3 * op, 13, op);
-        Drawer.drawRect(false, tiles.ax[ind] + rand2 * pos + 3 * op, tiles.ay[ind] + rand6 * pos, 13, 3 * op, op);
-        Drawer.drawRect(false, tiles.ax[ind] + rand3 * pos + 16, tiles.ay[ind] + rand7 * pos + 3, 
-        	-3 * op, 13, op);
-        Drawer.drawRect(false, tiles.ax[ind] + rand4 * pos + 3 * (1 - op), tiles.ay[ind] + rand8 * pos + 16, 
-        	13, -3 * op, op);
-        return false;
-    }
     
-    RNG (x, y, seed, max) {
+    static RNG (x, y, seed, max) {
+        // TODO: make an actual peudoRNG generator
+        //   This one does a bunch of shit to the numbers to obscure the result, but it probably has some big bias
     	let X1 = x / 16 + 0.1725 * seed * y / 4 + 3.4, X2 = y / 16 + 0.096 * seed * x / 4 + 3.2;
         let out = (X1 ^ X2) * (X1 + X2) + (X1 & X2) * (X1 * X2) + (X1 | X2) * Math.min(X1, X2);
         return Math.floor(out) % max;
     }
 }
+
+TileAnimation.animate = [];
+TileAnimation.animate[TILE_ANIM.BLUE] = function (dt, ind) {
+    return TileAnimation.shrink(dt, ind, COLOR.BLUE);
+};
+TileAnimation.animate[TILE_ANIM.RED] = function (dt, ind) {
+    return TileAnimation.shrink(dt, ind, COLOR.RED);
+};
+TileAnimation.animate[TILE_ANIM.GREEN] = function (dt, ind) {
+    return TileAnimation.shrink(dt, ind, COLOR.GREEN);
+};
+TileAnimation.animate[TILE_ANIM.ORANGE] = function (dt, ind) {
+    let op = 1 - (tiles.atime[ind] / 512);
+    Drawer.lineWidth = 3;
+    if (op <= 0) { return true; }
+    Drawer.setLineWidth(3).setColor(Tile.outerColorTable[4]);
+    Drawer.drawRect(true, tiles.ax[ind], tiles.ay[ind], 16, 16, op);
+    return false;
+};
+TileAnimation.animate[TILE_ANIM.DOUBLE_BLUE] = function (dt, ind) {
+    return TileAnimation.shrinkFill(dt, ind, COLOR.BLUE);
+};
+TileAnimation.animate[TILE_ANIM.DOUBLE_RED] = function (dt, ind) {
+    return TileAnimation.shrinkFill(dt, ind, COLOR.RED);
+};
+TileAnimation.animate[TILE_ANIM.DOUBLE_ORANGE] = function (dt, ind) {
+    let op = 1 - (tiles.atime[ind] / 512);
+    if (op <= 0) { return true; }
+    Drawer.color = Tile.innerColorTable[8];
+    Drawer.drawRect(false, tiles.ax[ind], tiles.ay[ind], 16, 16, 0.75 * op);
+    return false;
+};
+TileAnimation.animate[TILE_ANIM.ORANGE_RED] = function (dt, ind) {
+    let op = 1 - (tiles.atime[ind] / 512);
+    if (op <= 0) { return true; }
+    Drawer.setLineWidth(5 * op + 3).setColor(Tile.innerColorTable[9]);
+    Drawer.drawRect(true, tiles.ax[ind], tiles.ay[ind], 16, 16, 0.75);
+    Drawer.setLineWidth(3).setColor(Tile.outerColorTable[9]);
+    Drawer.drawRect(true, tiles.ax[ind], tiles.ay[ind], 16, 16, op);
+    return false;
+};
+TileAnimation.animate[TILE_ANIM.ORANGE_GREEN] = function (dt, ind) {
+    let op = 1 - (tiles.atime[ind] / 512);
+    if (op <= 0) { return true; }
+    Drawer.setLineWidth(5 * op + 3).setColor(Tile.innerColorTable[10]);
+    Drawer.drawRect(true, tiles.ax[ind], tiles.ay[ind], 16, 16, 0.75);
+    Drawer.setLineWidth(3).setColor(Tile.outerColorTable[10]);
+    Drawer.drawRect(true, tiles.ax[ind], tiles.ay[ind], 16, 16, op);
+    return false;
+};
+TileAnimation.animate[TILE_ANIM.YELLOW] = function (dt, ind) {
+    return TileAnimation.shrink(dt, ind, COLOR.YELLOW);
+};
+TileAnimation.animate[TILE_ANIM.ICE] = function (dt, ind) {
+    let op = 1 - (tiles.atime[ind] / 512) / 2,
+        pos = 8 * tiles.atime[ind] / 512 / 2;
+    if (op <= 0) { return true; }
+    Drawer.setColor(Tile.outerColorTable[13]);
+    let rand1 = 2 * TileAnimation.RNG(tiles.ax[ind], tiles.ay[ind], 0, 2) - 1,
+        rand2 = 2 * TileAnimation.RNG(tiles.ax[ind], tiles.ay[ind], 1, 2) - 1,
+        rand3 = 2 * TileAnimation.RNG(tiles.ax[ind], tiles.ay[ind], 2, 2) - 1,
+        rand4 = 2 * TileAnimation.RNG(tiles.ax[ind], tiles.ay[ind], 3, 2) - 1,
+        rand5 = 2 * TileAnimation.RNG(tiles.ax[ind], tiles.ay[ind], 4, 2) - 1,
+        rand6 = 2 * TileAnimation.RNG(tiles.ax[ind], tiles.ay[ind], 5, 2) - 1,
+        rand7 = 2 * TileAnimation.RNG(tiles.ax[ind], tiles.ay[ind], 6, 2) - 1,
+        rand8 = 2 * TileAnimation.RNG(tiles.ax[ind], tiles.ay[ind], 7, 2) - 1;
+    Drawer.drawRect(false, tiles.ax[ind] + rand1 * pos, tiles.ay[ind] + rand5 * pos, 3 * op, 13, op);
+    Drawer.drawRect(false, tiles.ax[ind] + rand2 * pos + 3 * op, tiles.ay[ind] + rand6 * pos, 13, 3 * op, op);
+    Drawer.drawRect(false, tiles.ax[ind] + rand3 * pos + 16, tiles.ay[ind] + rand7 * pos + 3, 
+        -3 * op, 13, op);
+    Drawer.drawRect(false, tiles.ax[ind] + rand4 * pos + 3 * (1 - op), tiles.ay[ind] + rand8 * pos + 16, 
+        13, -3 * op, op);
+    return false;
+};
