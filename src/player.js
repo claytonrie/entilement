8b59f84e510cb814e24e2cc49558b4c9f08e4663
Claire Riegger <jonfieldthe2nd@gmail.com>
2020-04-29 18:03:16 -0500

    Refactor player.js
    Change player from anonymous class to object literal
    Make player contain a TransitObject instead of extend it
    Comment some of the code

--- src/player.js
+++ src/player.js
@@ -2,11 +2,17 @@ const TMULT = 256;
 class TransitObj {
     constructor (x, y, tx = x, ty = y, vx = 0, vy = 0) {
         const TIME_DUR = 256;
+        // The position of the object in transition
         this.pos = new Vec2(x, y);
+        // The target position of the transition
         this.tar = new Vec2(tx, ty);
+        // The amount of time the current transition period has taken
         this.time = 0;
         this.slopeVar = new Vec2(null, null);
+        // Vector between the initial start point and the target point
         this.distVar = new Vec2(tx - x, ty - y);
+        
+        // Set up the slope variable
         if (tx === x) {
             this.slopeVar.x = 0;
         } else {
@@ -17,9 +23,11 @@ class TransitObj {
         } else {
             this.slopeVar.y = vy / (ty - y) + 1 / TIME_DUR;
         }
-        this.atTarget = false;
+        
+        this.atTarget = false; // Whether the object is at its desination
     }
 
+    /// Gets the velocity at the current point of time and returns it as a Vec2
     getVel() {
         const TIME_DUR = 256;
         let velx, vely;
@@ -35,6 +43,7 @@ class TransitObj {
         return new Vec2(velx, vely);
     }
     
+    /// Stops the current transition and starts a new one
     resetTransition (atTarget = false) {
         const TIME_DUR = 256;
         if (!atTarget) {
@@ -59,6 +68,7 @@ class TransitObj {
         this.atTarget = atTarget;
     }
 
+    /// Calculates one dt millisecond step along the objects transition
     smoothTransition (dt) {
         const TIME_DUR = 256;
         if (this.time >= TIME_DUR) {
@@ -73,46 +83,44 @@ class TransitObj {
     }
 }
 
-var player = new (class extends TransitObj {
-    constructor (x = 0, y = 0) {
-        super(x, y);
-        this.canMove = true;
-        this.onDiagonal = false;
-        this.scoutResult = {
-            up:   SCOUT_CODE.VALID, down:  SCOUT_CODE.VALID,
-            left: SCOUT_CODE.VALID, right: SCOUT_CODE.VALID
-        };
-        this.scout = new Vec2(); // Scouting position
-        // this.currDisp = 0;
-        // this.moveDisp = 0;
-        // this.displayMoves = false;
-        // this.prevMoves = "";
-        // this.currMoves = "";
-    }
+var player = {
+    // Variables
+    to: new TransitObject(-8, -8),
+    canMove: true,
+    onDiagonal: false,
+    scoutResult: {
+        up:   SCOUT_CODE.VALID, down:  SCOUT_CODE.VALID,
+        left: SCOUT_CODE.VALID, right: SCOUT_CODE.VALID
+    },
+    scout: new Vec2(), // Scouting position
 
+    // Methods
     moveDirection (dir) {
+        // Check to see if the direction is 
         if (dir.x === 0) {
             if (dir.y === -1) {
                 if (this.scoutResult.up === SCOUT_CODE.INVALID) { return false; }
-                //this.currMoves += "w";
             } else if (dir.y === 1) {
                 if (this.scoutResult.down === SCOUT_CODE.INVALID) { return false; }
-                //this.currMoves += "s";
             }
         } else if (dir.x === -1) {
             if (this.scoutResult.left === SCOUT_CODE.INVALID) { return false; }
-            //player.currMoves += "a";
         } else if (dir.x === 1) {
             if (this.scoutResult.right === SCOUT_CODE.INVALID) { return false; }
-           //player.currMoves += "d";
+        } else {
+            throw new Error(`Invalid direction: ${dir}`);
         }
+        // Set-up undo
         UndoHandler.startMove();
-        Tile.onStep(tiles.getTile(this.tar.x, this.tar.y), dir);
-        let ret = Tile.onLand(tiles.getTile(this.tar.x, this.tar.y), dir);
+        // Run tile-type-based scripts
+        Tile.onStep(tiles.getTile(this.to.tar.x, this.to.tar.y), dir);
+        let ret = Tile.onLand(tiles.getTile(this.to.tar.x, this.to.tar.y), dir);
+        // Record whether we are on a diagonal tile
         if (ret < 0) { this.onDiagonal = true; }
         else { this.onDiagonal = false; }
-        return ret
-    }
+        // Return the tile land value
+        return ret;
+    },
     
     scoutAllMoves () {
     	let dir = new Vec2();
@@ -122,7 +130,7 @@ var player = new (class extends TransitObj {
         dir.y =  0;
         dir.x = -1; this.scoutResult.left  = this.scoutMove(dir);
         dir.x =  1; this.scoutResult.right = this.scoutMove(dir);
-    }
+    },
     
     scoutMove (dir) {
         let ret = Tile.onStepScout(tiles.getTile(this.scout.x, this.scout.y), dir);
@@ -133,87 +141,21 @@ var player = new (class extends TransitObj {
         ret = Tile.onLandScout(tiles.getTile(this.scout.x, this.scout.y), dir);
         UndoHandler.undoScout();
         return ret;
-    }
+    },
     
-    /*handlePreviousMoves() {
-        if (this.displayMoves) {
-            if (key.isBuffered(KEY.DISP_UP)) {
-                key.useBuffer(KEY.DISP_UP);
-                if (this.moveDisp > 0) {
-                    this.moveDisp -= 1;
-                }
-            } else if (key.isBuffered(KEY.DISP_DOWN)) {
-                key.useBuffer(KEY.DISP_DOWN);
-                if (this.prevMoves.length > 16 && 
-                        this.prevMoves.length - this.moveDisp > 16) {
-                    this.moveDisp += 1;
-                }
-            }
-        } else {
-            key.useBuffer(KEY.DISP_UP, KEY.DISP_DOWN);
-            this.moveDisp = 0;
-        }
-        if (key.isBuffered(KEY.DISP)) {
-            key.useBuffer(KEY.DISP);
-            this.displayMoves = !this.displayMoves;
-        }
-    }*/
-    
-    die () {
-        this.canMove = false;
-    }
+    die () { this.canMove = false; },
 
     draw () {
-        if (!this.atTarget) {
+        // draw a light circle at the destination
+        if (!this.to.atTarget) {
             Drawer.setLineWidth(1).setColor("#FFF");
-            Drawer.drawCirc(true, this.tar, 7, 0.5);
+            Drawer.drawCirc(true, this.to.tar, 7, 0.5);
         }
         
+        // draw the player at their appearing position
         Drawer.color = "#FFF";
-        Drawer.drawCirc(false, this.pos, 7, 0.333);
+        Drawer.drawCirc(false, this.to.pos, 7, 0.333);
         Drawer.setLineWidth(3).setColor("yellow")
-        Drawer.drawCirc(true, this.pos, 7);
+        Drawer.drawCirc(true, this.to.pos, 7);
     }
-    
-    /*drawPreviousMoves (op = 1, dt = 0) {
-        if (this.displayMoves) {
-            const DEL = dt * Math.ceil(Math.abs(this.currDisp - this.moveDisp)) / 256;
-            if (this.currDisp + DEL < this.moveDisp) {
-                this.currDisp += DEL;
-            } else if (this.currDisp < this.moveDisp) {
-                this.currDisp = this.moveDisp;
-            }
-            if (this.currDisp - DEL > this.moveDisp) {
-                this.currDisp -= DEL;
-            } else if (this.currDisp > this.moveDisp) {
-                this.currDisp = this.moveDisp;
-            }
-            
-            let temp = Drawer.op, MD = this.currDisp | 0;
-            Drawer.op = op;
-            Drawer.setColor("#000")
-                .drawRect(false, 256 - 16, 0, 16, 256, 0.5);
-            let i = this.prevMoves.length > 17 ? 16 :
-                (this.prevMoves.length - 1);
-            for (; i > 0; i -= 1) {
-                if (MD + i + 1 === this.prevMoves.length) {
-                    Drawer.color = "#E00";
-                } else {
-                    Drawer.color = "#FFF";
-                }
-                if (i === 16) {
-                    Drawer.op = op * (this.currDisp % 1);
-                } else {
-                    Drawer.op = op;
-                }
-                Drawer.directionTriangle(false, this.prevMoves[MD + i],
-                    256 - 8, 8 + 16 * i - 16 * (this.currDisp % 1), 10);
-            }
-            if (MD === 0) { Drawer.color = "#EE0"; }
-            Drawer.op = op * (1 - (this.currDisp % 1));
-            Drawer.customTriangle(false, this.prevMoves[MD],
-                256 - 8, 8 - 16 * (this.currDisp % 1), 10);
-            Drawer.op = temp;
-        }
-    }*/
-})(-8, -8);
+};
