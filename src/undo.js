41ed6ef516f22cef24188c55f140ca5a71ea4f87
Claire Riegger <jonfieldthe2nd@gmail.com>
2020-05-01 18:00:17 -0500

    Tweak undo.js
    Move constants to enum.js
    Change tabs to spaces

--- src/undo.js
+++ src/undo.js
@@ -1,5 +1,3 @@
-const UNDO_MAX = tile.max,
-      SCOUt_MAX = Math.max(tile.maxX,tile.maxY);
 var undo = {
     // Normal undo
     max: UNDO_MAX,
@@ -10,49 +8,49 @@ var undo = {
     tuCount: 0, // Tile update count
 
     // Scout undo
-    sMax: SCOUt_MAX,
-    sPosX:  new Uint8Array(SCOUt_MAX),
-    sPosY:  new Uint8Array(SCOUt_MAX),
-    sTypes: new Int8Array(SCOUt_MAX),
+    sMax: UNDO_SCOUT_MAX,
+    sPosX:  new Uint8Array(UNDO_SCOUT_MAX),
+    sPosY:  new Uint8Array(UNDO_SCOUT_MAX),
+    sTypes: new Int8Array(UNDO_SCOUT_MAX),
     sCount: 0,
     
     startMove() {
-		this.steps[this.stepCount] = this.tuCount;
+        this.steps[this.stepCount] = this.tuCount;
         this.stepCount += 1;
     },
     
     startScout() {
-    	this.sCount = 0;
+        this.sCount = 0;
         player.scout.x = player.to.tar.x;
         player.scout.y = player.to.tar.y;
     },
     
     addTileUpdate(otype, index) {
-    	this.indices[this.tuCount] = index;
+        this.indices[this.tuCount] = index;
         this.types[this.tuCount] = otype;
         this.tuCount += 1;
     },
     
     addScoutUpdate(otype, x, y) {
-    	this.sPosX[this.sCount] = x;
-    	this.sPosY[this.sCount] = y;
+        this.sPosX[this.sCount] = x;
+        this.sPosY[this.sCount] = y;
         this.sTypes[this.sCount] = otype;
         this.sCount += 1;
     },
     
     undoTurn() {
         if (this.stepCount === 0) {
-        	return false;
+            return false;
         }
         this.stepCount -= 1;
         let ind, bx, by;
         while (this.tuCount > this.steps[this.stepCount]) {
-        	this.tuCount -= 1;
-        	ind = this.indices[this.tuCount];
-    		bx = ind % tile.maxX;
-        	by = (ind / tile.maxX) | 0;
+            this.tuCount -= 1;
+            ind = this.indices[this.tuCount];
+            bx = ind % tile.maxX;
+            by = (ind / tile.maxX) | 0;
             tile.setTile(this.types[this.tuCount], 
-            	bx, by, false, false);
+                bx, by, false, false);
         }
         player.to.tar.x = 16 * bx + 8;
         player.to.tar.y = 16 * by + 8;
@@ -64,12 +62,12 @@ var undo = {
     undoScout() {
         let bx, by;
         while (this.sCount > 0) {
-        	this.sCount -= 1;
-        	bx = this.sPosX[this.sCount];
-    		by = this.sPosY[this.sCount];
+            this.sCount -= 1;
+            bx = this.sPosX[this.sCount];
+            by = this.sPosY[this.sCount];
             //console.log()
             tile.setTile(this.sTypes[this.sCount], 
-            	bx, by, false, false, false);
+                bx, by, false, false, false);
         }
         player.scout.x = player.to.tar.x;
         player.scout.y = player.to.tar.y;
